d49aae5477d23b9629cc93fc63df6cf134a14c89
jperezes <jperezes@cisco.com>
2018-01-08 17:59:32 +0000

    revert all reverts and try to fix the mess

--- server.js
+++ server.js
@@ -1,42 +1,26 @@
 let port = process.env.PORT || 1349;
-let sparklite = require("sparklite");
+let SparkBot = require("./routes/sparkRegNew.js");
 let botdomain = process.env.TOMY_URL;
-let CheckCurrency = require('./routes/crypto');
-let TestApi = require('./routes/temp.js')
-const binance = require('node-binance-api');
-
-console.log("bot domain: " + botdomain)
-console.log("bot key: " + process.env.TOMY_KEY)
-let checkCurrency = new CheckCurrency()
-let testApi = new TestApi()
-let sparkBot = new sparklite.SparkBot(process.env.TOMY_KEY, port, botdomain);
+let sparkBot = new SparkBot(process.env.TOMY_KEY, port, botdomain);
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
-sparkBot.printHelloWorld();
-sparkBot.initializeWeebHooks()
-sparkBot.on('message', function(event){
+
+sparkBot.sparkBotEmitter.on('message', function(event){
   console.log('I have received an event!!!')
    var mail = event.personEmail.split('@');
    console.log("message received + :" + JSON.stringify(event))
    if (mail[1] === "cisco.com"){
-
-     let message = "your room id is : " + event.roomId;
-     checkCurrency.getAllPairs(event.roomId,sparkBot)
      sparkBot.sendMessage(event.roomId,"test",function(){})
-     binance.prices(function(ticker){
-       sparkBot.sendMessage(event.roomId,ticker,function(){})
-     })
    } else {
-
      sparkBot.sendMessage(event.roomId, "Hi, sorry to tell you that but you're not allowed to proceed",function(){});
    }
   console.log(JSON.stringify(event));
 });
 
-sparkBot.on('rooms', function(event){
+sparkBot.sparkBotEmitter.on('rooms', function(event){
     console.log(JSON.stringify(event));
 });
 
-sparkBot.on('memberships', function (event){
+sparkBot.sparkBotEmitter.on('memberships', function (event){
     console.log(JSON.stringify(event));
 });
 
@@ -44,5 +28,5 @@ sparkBot.on('memberships', function (event){
 let bot = function(result){
   console.log(result);
 }
-testApi.listenForStadistics(sparkBot, sparkBot.getServer());
+//testApi.listenForStadistics(sparkBot, sparkBot.getServer());
 //checkCurrency.scheduleServer(bot);
