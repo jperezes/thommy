bbeab55bb0653850a178043e66ff6c4ba0f57c68
jperezes <jperezes@cisco.com>
2018-01-10 17:16:50 +0000

    add the pair

--- routes/crypto.js
+++ routes/crypto.js
@@ -36,7 +36,7 @@ async function checkLastPairPrice(pair) {
   let data = Object.assign({},{symbol:pair})
   try {
     console.log("object 1 is: " + JSON.stringify(data))
-    let result = await binanceRest.tickerPrice()
+    let result = await binanceRest.tickerPrice(pair)
     return parseFloat(result.price);
   }catch(e) {
     console.log("error checking price" + e)
